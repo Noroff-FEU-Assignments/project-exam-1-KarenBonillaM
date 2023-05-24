4e9156678a9393f91f0318a594962f6e0aa7632e
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2023-05-24 18:27:44 +0200

    24 may

--- js/index.js
+++ js/index.js
@@ -32,3 +32,44 @@ const dragStop = () => {
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
+
+
+//VALIDATE EMAIL 
+const buttonSingUp = document.querySelector(".cta-signup");
+const emailSubscribe = document.querySelector("#subscribe-email");
+const subscribeError = document.querySelector("#subscribeError");
+const subscribeSuccess = document.querySelector("#subscribe-success");
+
+
+
+function validateSubscribeInput() {
+  event.preventDefault();
+
+  //VALIDATE EMAIL
+  if(validateEmail(emailSubscribe.value) === true) {
+    subscribeError.style.display = "none";
+  } else {
+    subscribeError.style.display = "block";
+  }
+
+if((validateEmail(emailSubscribe.value) === true)) {
+  subscribeSuccess.style.display = "block";
+} else {
+  subscribeSuccess.style.display = "none";
+}
+}
+
+buttonSingUp.addEventListener("click", validateSubscribeInput);
+
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}
+
+const closeIcon = document.querySelector(".close-icon");
+
+closeIcon.addEventListener("click", function(){
+  subscribeSuccess.style.display = "none"
+})
